49f1274c302c5e1433eae156de087cfa78af8af7
Ronaldo Ramos <rangerskiper@gmail.com>
2022-10-16 17:39:01 -0400

    Add migration Pedido and relationship hasMany between

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nombre',20);
+            $table->string('apellidos',20);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('rol',10);
+            $table->string('image',100);
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2022_10_16_205652_create_pedidos_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePedidosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pedidos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('provincia',50);
+            $table->string('localidad',50);
+            $table->string('direccion',40);
+            $table->float('coste');
+            $table->string('estado',20);
+            $table->date('fecha');
+            $table->time('hora');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pedidos');
+    }
+}

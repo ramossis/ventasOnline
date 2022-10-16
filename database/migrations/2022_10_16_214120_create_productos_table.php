309121420d236779c1b8da51e75eaff10c8c579f
Ronaldo Ramos <rangerskiper@gmail.com>
2022-10-16 18:10:17 -0400

    Add migration Categoria, Producto and relationship hasMany on between

---
+++ database/migrations/2022_10_16_214120_create_productos_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('categoria_id');
+            $table->string('nombre',20);
+            $table->text('descripcion');
+            $table->float('precio');
+            $table->integer('stock')->unsigned();
+            $table->float('oferta');
+            $table->date('fecha');
+            $table->string('imagen');
+            $table->foreign('categoria_id')->references('id')->on('categorias');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
